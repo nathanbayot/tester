37d0880cc467a058fdcb449266baf069450880f8
21402 <21402@wc.school.nz>
2025-02-11 12:27:28 +1300

    test 2

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
-print("im nathan")
 print("whats up")
+print("im nathan")
 nathan = input("whats your name")
 if nathan == "cool":
     print("nice")
+print("")
+print("I want to play football")
+print("do you want to play")
+try:
+    play_football = int(input("1 for yes and 0 for no"))
+except ValueError:
+    print("only numbers")
+    play_football = int(input("1 for yes and 0 for no"))
+
+if play_football > 1:
+    print("try again")
+    play_football = int(input("1 for yes and 0 for no"))
+if play_football == 1:
+    print("lets play")
+elif play_football == 0:
+    print("sad for you")
